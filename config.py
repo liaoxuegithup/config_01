00cf883b9d53bf15c7ea13e4ed7c025c0d50f92c
liaoxuegithup <1335992113@qq.com>
2018-05-25 08:52:47 +0800

    封装开发环境配置信息

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from redis import StrictRedis
+from unittest import TestCase
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_01"
@@ -13,3 +14,19 @@ class Config(object):
     SESSION_USE_SIGNR = True
 #     开启31天
     PERMANENT_SESSION_LIFETIME = 60*60*24
+# 定义醒目的生产环境,保证项目在哪个环境下都不出错,生产环境有3个
+# 开发环境(写代码),生产环境(准备上线,将所有代码链接在一起),测试环境(在上线之前检查代码是否有问题)
+# 需要定义三个环境的目的是不同环境在整个项目中配置不同,基于config
+class Develop(Config):
+    # 开发环境基本基于config
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_01"
+    pass
+
+class Production(Config):
+    # 生产环境将所有代码封装的过程,不需要测试
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_pro_01"
+
+class TestEnvironment(TestCase):
+    # 测试环境TestCase里面封装了debug
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_test_01"
