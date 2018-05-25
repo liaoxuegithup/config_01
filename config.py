2b97d6691746782309255d0579e41b59891215d2
liaoxuegithup <1335992113@qq.com>
2018-05-25 10:54:56 +0800

    配置并封装日志记录

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from redis import StrictRedis
 from unittest import TestCase
-
+import logging
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_01"
@@ -18,18 +18,20 @@ class Config(object):
 # 定义醒目的生产环境,保证项目在哪个环境下都不出错,生产环境有3个
 # 开发环境(写代码),生产环境(准备上线,将所有代码链接在一起),测试环境(在上线之前检查代码是否有问题)
 # 需要定义三个环境的目的是不同环境在整个项目中配置不同,基于config
+# 在每个开发环境下都需要用日志记录信息,很容易的观察哪里是否有错误
 class Develop(Config):
     # 开发环境基本基于config
-
+    LOG_LEVE = logging.DEBUG
     pass
 
 class Production(Config):
     # 生产环境将所有代码封装的过程,不需要测试
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_pro_01"
-
+    LOG_LEVE = logging.DEBUG
 class TestEnvironment(TestCase):
     # 测试环境TestCase里面封装了debug
+    LOG_LEVE = logging.DEBUG
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_test_01"
     # 修改父类的配置信息后,还需要保证在运行的时候app.config里面的配置信息在改变,即运行生产环境就放生产环境的,开发就放开发的配置信息
     # 所以在init里面app的参数是变动的,可以以传参的方式向app里面传变量因为是向app中传不同的参数,app不能动,所以需要将app封装起来
