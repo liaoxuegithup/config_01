89ef7a9a7050615de3e4045361e33859d3e02f85
liaoxuegithup <1335992113@qq.com>
2018-05-25 11:14:35 +0800

    提交logs文件夹

--- config.py
+++ config.py
@@ -1,43 +1,108 @@
+# from redis import StrictRedis
+# from unittest import TestCase
+# import logging
+# class Config(object):
+#     DEBUG = True
+#     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_01"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = "fkdfkd"
+#     REDIS_HOST = "127.0.0.1"
+#     REDIS_POST = 6379
+#     SESSION_TYPE = "redis"
+# #   设置SESSION设置在redis库中
+#     SESSION_REDIS = StrictRedis(host= REDIS_HOST)
+# #     指定Session存储到后端的位置c,
+#     SESSION_USE_SIGNR = True
+# #     开启31天
+#     PERMANENT_SESSION_LIFETIME = 60*60*24
+# # 定义醒目的生产环境,保证项目在哪个环境下都不出错,生产环境有3个
+# # 开发环境(写代码),生产环境(准备上线,将所有代码链接在一起),测试环境(在上线之前检查代码是否有问题)
+# # 需要定义三个环境的目的是不同环境在整个项目中配置不同,基于config
+# # 在每个开发环境下都需要用日志记录信息,很容易的观察哪里是否有错误
+# class Develop(Config):
+#     # 开发环境基本基于config
+#     LOG_LEVE = logging.DEBUG
+#     pass
+#
+# class Production(Config):
+#     # 生产环境将所有代码封装的过程,不需要测试
+#     DEBUG = False
+#     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_pro_01"
+#     LOG_LEVE = logging.DEBUG
+# class TestEnvironment(TestCase):
+#     # 测试环境TestCase里面封装了debug
+#     LOG_LEVE = logging.DEBUG
+#     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_test_01"
+#     # 修改父类的配置信息后,还需要保证在运行的时候app.config里面的配置信息在改变,即运行生产环境就放生产环境的,开发就放开发的配置信息
+#     # 所以在init里面app的参数是变动的,可以以传参的方式向app里面传变量因为是向app中传不同的参数,app不能动,所以需要将app封装起来
+#     # 可以使用basconvert字典的形式,将类名传过去info那边可以根据以建取值类名的方式,执行方法,basconvert里封装了类名可以调用对应的属性
+# config_environment_app = {
+#     'dev':Develop,
+#     'pro' :Production,
+#     'test':TestEnvironment,
+# }
+# -------------------------------------------------------
 from redis import StrictRedis
-from unittest import TestCase
 import logging
-class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_01"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "fkdfkd"
-    REDIS_HOST = "127.0.0.1"
-    REDIS_POST = 6379
-    SESSION_TYPE = "redis"
-#   设置SESSION设置在redis库中
-    SESSION_REDIS = StrictRedis(host= REDIS_HOST)
-#     指定Session存储到后端的位置c,
-    SESSION_USE_SIGNR = True
-#     开启31天
-    PERMANENT_SESSION_LIFETIME = 60*60*24
-# 定义醒目的生产环境,保证项目在哪个环境下都不出错,生产环境有3个
-# 开发环境(写代码),生产环境(准备上线,将所有代码链接在一起),测试环境(在上线之前检查代码是否有问题)
-# 需要定义三个环境的目的是不同环境在整个项目中配置不同,基于config
-# 在每个开发环境下都需要用日志记录信息,很容易的观察哪里是否有错误
-class Develop(Config):
-    # 开发环境基本基于config
-    LOG_LEVE = logging.DEBUG
-    pass
 
-class Production(Config):
-    # 生产环境将所有代码封装的过程,不需要测试
+
+class Config(object):
+    """配置文件的加载"""
+
+    # 项目秘钥：CSRF/session,还有其他的一些签名算法会用
+    SECRET_KEY = 'q7pBNcWPgmF6BqB6b5VICF7z7pI+90o0O4CaJsFGjzRsYiya9SEgUDytXvzFsIaR'
+
+    # 开启调试模式
+    DEBUG = True
+
+    # 配置MySQL数据库连接信息:真实开发中，要使用mysql数据库的真实IP
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_29'
+    # 不去追踪数据库的修改，节省开销
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 配置redis数据库:因为redis模块不是flask的扩展，所以就不会自动的从config中读取配置信息，只能自己读取
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    # 指定session使用什么来存储
+    SESSION_TYPE = 'redis'
+    # 指定session数据存储在后端的位置
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 是否使用secret_key签名你的sessin
+    SESSION_USE_SIGNER = True
+    # 设置过期时间，要求'SESSION_PERMANENT', True。而默认就是31天
+    PERMANENT_SESSION_LIFETIME = 60*60*24 # 一天有效期
+
+
+# 以下代码是封装不同开发环境下的配置信息
+
+class DevlopmentConfig(Config):
+    """开发环境"""
+    # 开发环境和父类基本一致
+
+    # 开发环境日志等级
+    LEVEL_LOG = logging.DEBUG
+
+
+class ProductionConfig(Config):
+    """生产环境"""
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_pro_01"
-    LOG_LEVE = logging.DEBUG
-class TestEnvironment(TestCase):
-    # 测试环境TestCase里面封装了debug
-    LOG_LEVE = logging.DEBUG
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/config_test_01"
-    # 修改父类的配置信息后,还需要保证在运行的时候app.config里面的配置信息在改变,即运行生产环境就放生产环境的,开发就放开发的配置信息
-    # 所以在init里面app的参数是变动的,可以以传参的方式向app里面传变量因为是向app中传不同的参数,app不能动,所以需要将app封装起来
-    # 可以使用basconvert字典的形式,将类名传过去info那边可以根据以建取值类名的方式,执行方法,basconvert里封装了类名可以调用对应的属性
-config_environment_app = {
-    'dev':Develop,
-    'pro' :Production,
-    'test':TestEnvironment,
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_pro_29'
+    # 生产环境日志等级
+    LEVEL_LOG = logging.ERROR
+
+
+class UnittestConfig(Config):
+    """测试环境"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_case_29'
+    # 生产环境日志等级
+    LEVEL_LOG = logging.DEBUG
+
+
+# 定义字典，存储关键字对应的不同的配置类的类名
+configs = {
+    'dev':DevlopmentConfig,
+    'pro':ProductionConfig,
+    'unit':UnittestConfig
 }
--- info/__init__.py
+++ info/__init__.py
@@ -1,11 +1,57 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from redis import StrictRedis
+# from flask_wtf.csrf import CSRFProtect
+# from flask_session import Session
+# from config import Config,config_environment_app
+# import logging
+# from logging.handlers import RotatingFileHandler
+# def setup_log(level):
+#     """根据创建app时的配置环境，加载日志等级"""
+#     # 设置日志的记录等级
+#     logging.basicConfig(level=level)  # 调试debug级
+#     # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+#     file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+#     # 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+#     formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+#     # 为刚创建的日志记录器设置日志记录格式
+#     file_log_handler.setFormatter(formatter)
+#     # 为全局的日志工具对象（flask app使用的）添加日志记录器
+#     logging.getLogger().addHandler(file_log_handler)
+# logs文件夹需要提交,但是log文件不需要提交,错误信息不需要别人知道
+# 在push时git规定不能传空的文件夹,git规定在文件夹内创建.gitkeep文件
+#
+#
+# db = SQLAlchemy()
+# def fenzhang(config_environment_app):
+#     app = Flask(__name__)
+#     app.config.from_object(config_environment_app["pro"])
+#     db.init_app("bianglaing")
+#
+#     # 以后都是以类的方式封装，方便修改和管理
+#     # 迁移管理和app相关联
+#     str = StrictRedis(host = config_environment_app["pro"].REDIS_HOST,post = config_environment_app["pro"].REDIS_POST)
+#     # 将redis的地址和端口号放到app的配置文件中
+#     # 那个环境下需要需要配置不同的redis,链接不同redis数据库,但是是否组要在不同环境下更改redis的ip吗
+#     # 开启CSRF保护
+#     CSRFProtect(app)
+# # 需要生成密钥
+#     return app
+# # 问题是db怎样传过去
+# # 解决办法是,在底层SQLAlchemy类直接调用init方法,将app以传参的方式,init直接调用app,可以给SQLAlchemy创建对象,用对象调用init方法来调用app
+# # 来解决每个环境都需要开启数据库的方式,那么为什么,manager里面不需要传,拥有环境配置信息的SQLAlchemy呢?(因为app传过去是给manager里面配置app的配置信息)
+#------------------------------------------------------------------------
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
-from config import Config,config_environment_app
+# from config import Config, DevlopmentConfig, ProductionConfig, UnittestConfig
+from config import configs
 import logging
 from logging.handlers import RotatingFileHandler
+
+
 def setup_log(level):
     """根据创建app时的配置环境，加载日志等级"""
     # 设置日志的记录等级
@@ -20,21 +66,34 @@ def setup_log(level):
     logging.getLogger().addHandler(file_log_handler)
 
 
+# 创建SQLAlchemy对象
 db = SQLAlchemy()
-def fenzhang(config_environment_app):
-    app = Flask(__name__)
-    app.config.from_object(config_environment_app["pro"])
-    db.init_app("bianglaing")
 
-    # 以后都是以类的方式封装，方便修改和管理
-    # 迁移管理和app相关联
-    str = StrictRedis(host = config_environment_app["pro"].REDIS_HOST,post = config_environment_app["pro"].REDIS_POST)
-    # 将redis的地址和端口号放到app的配置文件中
-    # 那个环境下需要需要配置不同的redis,链接不同redis数据库,但是是否组要在不同环境下更改redis的ip吗
-    # 开启CSRF保护
+
+def create_app(config_name):
+    """创建app的工厂方法
+    参数：根据参数选择不同的配置类
+    """
+
+    # 根据创建app时的配置环境，加载日志等级
+    setup_log(configs[config_name].LEVEL_LOG)
+
+    app = Flask(__name__)
+
+    # 获取配置信息
+    app.config.from_object(configs[config_name])
+
+    # 创建连接到MySQL数据库的对象
+    # db = SQLAlchemy(app)
+    db.init_app(app)
+
+    # 创建连接到redis数据库的对象
+    redis_store = StrictRedis(host=configs[config_name].REDIS_HOST, port=configs[config_name].REDIS_PORT)
+
+    # 开启CSRF保护：因为项目中的表单不再使用FlaskForm来实现，所以不会自动的开启CSRF保护，需要自己开启
     CSRFProtect(app)
-# 需要生成密钥
+
+    # 指定session数据存储在后端的位置
+    Session(app)
+
     return app
-# 问题是db怎样传过去
-# 解决办法是,在底层SQLAlchemy类直接调用init方法,将app以传参的方式,init直接调用app,可以给SQLAlchemy创建对象,用对象调用init方法来调用app
-# 来解决每个环境都需要开启数据库的方式,那么为什么,manager里面不需要传,拥有环境配置信息的SQLAlchemy呢?(因为app传过去是给manager里面配置app的配置信息)
